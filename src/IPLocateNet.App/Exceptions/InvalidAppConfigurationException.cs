a5e778ef372e128d5b3f2b915981aa6dfca6fe76
GHCentre <ghcentre@gmail.com>
2026-07-24 21:56:07 +0300

    Add DI configurator for Data layer

---
+++ src/IPLocateNet.App/Exceptions/InvalidAppConfigurationException.cs
@@ -0,0 +1,12 @@
+﻿namespace IPLocateNet.App.Exceptions;
+
+public class InvalidAppConfigurationException : InvalidOperationException
+{
+    public InvalidAppConfigurationException(string? message) : base(message)
+    {
+    }
+
+    public InvalidAppConfigurationException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
---
+++ src/IPLocateNet.Inf.Data.DI/DataLayerConfigurator.cs
@@ -0,0 +1,33 @@
+﻿using IPLocateNet.App.Exceptions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IPLocateNet.Inf.Data.DI;
+
+public static class DataLayerConfigurator
+{
+    public static void ConfigureServices(IServiceCollection services, Func<string?> connectionStringFactory)
+    {
+        services.AddDbContext<AppDbContext>(
+            (sp, options) =>
+            {
+                if (connectionStringFactory() is not string connectionString || string.IsNullOrWhiteSpace(connectionString))
+                {
+                    throw new InvalidAppConfigurationException("Connection string is empty.");
+                }
+
+                options.UseSqlite(
+                    connectionString,
+                    options =>
+                    {
+                    });
+            });
+    }
+
+    public static async Task ConfigureHostAsync(IServiceProvider sp)
+    {
+        using var scope = sp.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        await db.Database.MigrateAsync();
+    }
+}
